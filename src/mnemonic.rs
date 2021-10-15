b572ba7420808689fda33d57704244408915f6c7
Taiki Harada <haradataiki19990927@gmail.com>
2021-10-15 11:26:45 +0900

    Implement traits.

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -1,27 +1,32 @@
 use crate::register::Register;
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum Instruction {
     I(InstructionI),
     R(InstructionR),
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct InstructionI {
     pub opcode: Opcode,
     pub dst: Register,
     pub immediate: i8,
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct InstructionR {
     pub funct: Funct,
     pub dst: Register,
     pub src: Register,
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum Opcode {
     LDI,
     ADDI,
 }
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum Funct {
     LD,
     ST,
