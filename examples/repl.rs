038105a2ad249b619715c9b351549f090a1c053d
Taiki Harada <haradataiki19990927@gmail.com>
2021-10-15 10:53:09 +0900

    Implement repl.

---
+++ examples/repl.rs
@@ -0,0 +1,32 @@
+use pasm::{ast::*, lexer::*, parser::*, token::*};
+use std::io::{stdout, Write};
+
+use lalrpop_util::ParseError;
+
+fn main() -> std::io::Result<()> {
+    loop {
+        print!(">> ");
+        stdout().flush()?;
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+
+        match input.as_str().trim() {
+            "quit" | ":q" => break,
+            input => match parse(input) {
+                Ok(res) => println!("{:#?}", &res),
+                Err(err) => {
+                    println!("Parse Error");
+                    println!("{:#?}", &err);
+                },
+            },
+        }
+    }
+
+    Ok(())
+}
+
+fn parse(input: &str) -> Result<Instruction, ParseError<(), Token, ()>> {
+    let lexer = Lexer::new(input);
+    poco::InstructionParser::new().parse(lexer)
+}
